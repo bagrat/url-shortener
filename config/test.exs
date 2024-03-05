4a9e3caf45906e7522959e366a8e8abf148e1611
Bagrat Aznauryan <contact@bagrat.io>
2024-03-05 06:38:05 +0400

    Add DB in Docker Compose

--- config/test.exs
+++ config/test.exs
@@ -9,6 +9,7 @@ config :shortener, Shortener.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
+  port: 54320,
   database: "shortener_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
